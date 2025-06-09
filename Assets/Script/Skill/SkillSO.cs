014a4667fc6545523f7907b571093bda81afa3be
LeeChangju <dlfclao@gmail.com>
2025-06-09 09:35:50 +0900

    refactor: Restructure SkillSO to align with intended design architecture

--- Assets/Script/Skill/SkillSO.cs
+++ Assets/Script/Skill/SkillSO.cs
@@ -1,10 +1,6 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
 using UnityEngine;
-
-public enum ESkillType
-{
-    Weapon,
-    Passive
-}
 
 public abstract class SkillSO : ScriptableObject
 {
@@ -14,8 +10,8 @@ public abstract class SkillSO : ScriptableObject
     public Sprite icon;
 }
 
-[CreateAssetMenu(menuName = "Skill/ActiveSkill")]
-public class ActiveSkillSO : SkillSO
+[CreateAssetMenu(menuName = "Skill/WeaponActiveSkill")]
+public class WeaponActiveSkillSO : SkillSO
 {
     [Header ("Stats")]
     public float cooldown = 5f;
@@ -27,12 +23,60 @@ public class ActiveSkillSO : SkillSO
     public AudioClip castSound;
 }
 
+public class WeaponPassiveSkillSO : SkillSO
+{
+}
+
+#region Passive Effect SO
+public abstract class PassiveEffectSO : ScriptableObject
+{
+    public string effectName;
+    [TextArea] public string description;
+    public Sprite icon;
+
+    [Header("Trigger Settings")]
+    public float cooldown = 0f;
+    protected float lastActivatedTime = -999f;
+
+    public virtual bool CanTrigger(PlayerStatus status)
+    {
+        return Time.time >= lastActivatedTime + cooldown;
+    }
+
+    public virtual void OnEventTriggered(PlayerStatus status)
+    {
+        if (CanTrigger(status))
+        {
+            Apply(status);
+            lastActivatedTime = Time.time;
+        }
+    }
+
+    public abstract void Apply(PlayerStatus status);
+}
+#endregion
+
+#region Damage Reduction Passive Effect SO
+[CreateAssetMenu(menuName = "PassiveEffect/DamageReductionOnHit")]
+public class DamageReductionPassiveSO : PassiveEffectSO
+{
+    public float duration = 5f;
+    public float reductionPercent = 0.3f;
+
+    public override void Apply(PlayerStatus status)
+    {
+        status.ApplyTemporaryDamageReduction(reductionPercent, duration, triggerCount);
+    }
+}
+#endregion
+
+#region Armor Passive Skill
 [CreateAssetMenu(menuName = "Skill/ArmorPassiveSkill")]
 public class ArmorPassiveSkillSO : SkillSO
 {
-    public int bonusDefense;
-    public float damageReduction;
+    public List<PassiveEffectSO> passiveEffects;
 }
+#endregion
 
 [CreateAssetMenu(menuName = "Skill/AccessoryPassiveSkill")]
 public class AccessoryPassiveSkillSO : SkillSO
